57cfa7b574975710290f4703eb8b27bd7d062964
wayllertt <alidzhieva2005@mail.ru>
2026-02-06 18:28:03 +0300

    + graphql

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,14 +3,40 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+
+	"posts-comments-1/graph"
+	"posts-comments-1/internal/storage/memory"
+	pg "posts-comments-1/internal/storage/postgres"
 )
 
 func main() {
-	mux := http.NewServeMux()
+	storageType := os.Getenv("STORAGE_TYPE")
 
+	var resolver graph.Resolver
+	if storageType == "postgres" {
+		pgs, err := pg.New()
+		if err != nil {
+			log.Fatal(err)
+		}
+		resolver = graph.Resolver{Storage: pgs}
+	} else {
+		resolver = graph.Resolver{Storage: memory.New()}
+	}
+
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{
+		Resolvers: &resolver,
+	}))
+
+	mux := http.NewServeMux()
+	mux.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	mux.Handle("/query", srv)
 	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("ok"))
+		_, _ = w.Write([]byte("ok"))
 	})
 
 	log.Println("server started on :8080")
